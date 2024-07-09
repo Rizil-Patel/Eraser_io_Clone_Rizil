4f1248b6b3288271aed1b0af0cf686f15fa6bcbb
Rizil PATEL <128392275+Rizil-Patel@users.noreply.github.com>
2024-07-09 15:56:44 +0530

    Update Header.tsx

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -15,7 +15,7 @@ function Header() {
     <div className="flex flex-1 items-center justify-end md:justify-between">
       <nav aria-label="Global" className="hidden md:block">
         <ul className="flex items-center gap-6 text-sm">
-          <li>
+{/*           <li>
             <a className="text-white transition hover:text-gray-100" href="#"> About </a>
           </li>
 
@@ -33,7 +33,7 @@ function Header() {
 
           <li>
             <a className="text-white transition hover:text-gray-100/75" href="#"> Projects </a>
-          </li>
+          </li> */}
 
         
         </ul>
